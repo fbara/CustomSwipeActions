58160938859276e10e9603f2cb16a9bb50a93169
fbara <fbara1@yahoo.com>
2024-09-13 15:05:44 -0400

    Initial Commit

---
+++ CustomSwipeActions/CustomSwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwipeActionsApp.swift
+//  CustomSwipeActions
+//
+//  Created by Frank Bara on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
